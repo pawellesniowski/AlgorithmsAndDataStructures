11df208ed6556ef0061d5ce24727eefd10b58b69
pawel lesniowski <pawel.html@gmail.com>
2019-12-07 21:36:18 +0100

    reverse a string recursively

--- AlgorithmsAndData2.js
+++ AlgorithmsAndData2.js
@@ -1,18 +1,41 @@
-// Fiz buzz to warm up.
+"use strict";
+
+//
+// 0. Fiz buzz to warm up.
 function fizBazz(number) {
-    if (typeof number !== 'number' || Object.is(number, NaN)) {
-        console.log('This is not a numeric value.')
-        return false;
-    }
-    if (number % 3 == 0) {
-        console.log('Fizz')
-    } else if (number % 5 == 0) {
-        console.log("Buzz")
-    } else {
-        console.log(number);
-    }
-    return true;
+  if (typeof number !== "number" || Object.is(number, NaN)) {
+    console.log("This is not a numeric value.");
+    return false;
+  }
+  if (number % 3 == 0) {
+    console.log("Fizz");
+  } else if (number % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(number);
+  }
+  return true;
 }
+// fizBazz();
 
-fizBazz([]);
-
+//
+// 1. Reverse a string - recursion.
+function reverseString(str) {
+  if (typeof str == "string") {
+    let reversed = "";
+    function reverse(str) {
+      if (str.length == 0) {
+        return;
+      }
+      reversed += str.slice(-1);
+      reverse(str.slice(0, str.length - 1));
+    }
+    reverse(str);
+    console.log("reversed: ", reversed);
+    return reversed;
+  } else {
+    console.log("Argument is not a string");
+    return false;
+  }
+}
+reverseString("apple");
