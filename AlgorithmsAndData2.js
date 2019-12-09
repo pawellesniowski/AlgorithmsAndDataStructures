7fd4d6d3cc63ad08dc2212fd1b957e662f154c7f
Pawel_Lesniowski <pawel_lesniowski@epam.com>
2019-12-09 20:33:05 +0100

    palindrome

--- AlgorithmsAndData2.js
+++ AlgorithmsAndData2.js
@@ -3,62 +3,82 @@
 //
 // 0. Fiz buzz to warm up.
 function fizBazz(number) {
-  if (typeof number !== "number" || Object.is(number, NaN)) {
-    console.log("This is not a numeric value.");
-    return false;
-  }
-  if (number % 3 == 0) {
-    console.log("Fizz");
-  } else if (number % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(number);
-  }
-  return true;
+    if (typeof number !== "number" || Object.is(number, NaN)) {
+        console.log("This is not a numeric value.");
+        return false;
+    }
+    if (number % 3 == 0) {
+        console.log("Fizz");
+    } else if (number % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(number);
+    }
+    return true;
 }
 // fizBazz();
 
 //
 // 1.1 Reverse a string - recursively.
 function reverseString(str) {
-  if (typeof str == "string") {
-    let reversed = "";
-    function reverse(str) {
-      if (str.length == 0) {
-        return;
-      }
-      reversed += str.slice(-1);
-      reverse(str.slice(0, str.length - 1));
+    if (typeof str == "string") {
+        let reversed = "";
+        function reverse(str) {
+            if (str.length == 0) {
+                return;
+            }
+            reversed += str.slice(-1);
+            reverse(str.slice(0, str.length - 1));
+        }
+        reverse(str);
+        console.log("reversed: ", reversed);
+        return reversed;
+    } else {
+        console.log("Argument is not a string");
+        return false;
     }
-    reverse(str);
-    console.log("reversed: ", reversed);
-    return reversed;
-  } else {
-    console.log("Argument is not a string");
-    return false;
-  }
 }
 // reverseString("apple");
 
 //
 // 1.2. Reverse string with for of loop.
 function reverseStringLoop(str) {
-  let reversed = "";
-  for (let letter of str) {
-    reversed = letter + reversed;
-  }
-  console.log("reversed: ", reversed);
-  return reversed;
+    let reversed = "";
+    for (let letter of str) {
+        reversed = letter + reversed;
+    }
+    console.log("reversed: ", reversed);
+    return reversed;
 }
 // reverseStringLoop("banana");
 
 //
 // 1.3. Reverse string with reduce.
 function reverseStringWithReduce(str) {
-  const reversedArr = str.split("").reduce((acc, char) => {
-    acc = char + acc;
-    return acc;
-  }, "");
-  console.log(reversedArr);
+    const reversedArr = str.split("").reduce((acc, char) => {
+        acc = char + acc;
+        return acc;
+    }, "");
+    console.log(reversedArr);
 }
-reverseStringWithReduce("Orange");
+// reverseStringWithReduce("Orange");
+
+
+//
+// 2. Palindromes
+function isPalindrome(str) {
+    if (typeof str !== 'string') {
+        return 'not a string';
+    }
+    const toReverse = str.toLowerCase();
+    let reversed = '';
+    for (let char of toReverse) {
+        reversed = char + reversed;
+    }
+    console.log(reversed);
+    return reversed == toReverse;
+}
+// console.log(isPalindrome('Appa'));
+
+//
+// 
